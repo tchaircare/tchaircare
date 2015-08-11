42a4e0ce97c98ab6cdf2386a53d863bfb33d4af8
Camden Rudisill <camden.rudisill@richmond.edu>
2015-08-10 23:41:05 -0400

    Update: rebuilt gulp file, added src and build directories

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,44 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	uglifycss = require('gulp-csso'),
 	imagemin = require('gulp-imagemin'),
+	cache = require('gulp-cache');
 	concat = require('gulp-concat'),
 	concatCSS = require('gulp-concat-css');
+	rename = require('gulp-rename');
 
-// HTML - Minify
+	var src = 'src/';
+	var dest = 'build/';
+
+// Index - Minify
+gulp.task('minify-index', function() {
+	var opts = {
+		conditionals: true,
+		spare: true
+	};
+
+	return gulp.src(['./main.html'])
+		.pipe(minifyHTML(opts))
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('./'));
+	});
+
+// HTML - Minify /views
 gulp.task('minify-html', function() {
 	var opts = {
 		conditionals: true,
 		spare: true
 	};
 
-	return gulp.src(['./index.html','views/careers.html', 'views/staff.html'])
+	return gulp.src(['src/views/careers.html', 'src/views/staff.html'])
 		.pipe(minifyHTML(opts))
-		.pipe(gulp.dest('./build/'));
-	})
+		.pipe(gulp.dest('./build/views'));
+	});
 
 // CSS -> Remove Unused, minify, concat
 gulp.task('styles', function(){
-	return gulp.src(['css/bootstrap.min.css', 'css/custom.css'])
+	return gulp.src(['src/css/bootstrap.min.css', 'src/css/custom.css'])
 		.pipe(uncss({
-			html: ['index.html', 'views/*.html'], ignore: [
+			html: ['./index.html', 'src/views/*.html'], ignore: [
             ".fade",
             ".fade.in",
             ".collapse",
@@ -40,14 +58,24 @@ gulp.task('styles', function(){
 
 // Compress Images
 gulp.task('image', function(){
-	gulp.src('img/*')
-		.pipe(imagemin())
+	gulp.src('src/img/**/*')
+		.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
 		.pipe(gulp.dest('build/img'));	
 	});
 
-
+// Watch for changes in files
+gulp.task('watch', function() {
+	// watch index.html
+	gulp.watch('main.html', ['minify-index']);
+	// watch views/.html
+	gulp.watch(src + 'views/*.html', ['minify-html']);
+	// watch .css files
+	gulp.watch(src + 'css/*.css', ['styles']);
+	// watch image files
+	gulp.watch(src + 'img/**/*', ['image']);
+	});
 
 
 // allows you to just type 'gulp' in terminal
-gulp.task('default', ['minify-html', 'styles', 'image']); 
+gulp.task('default', ['watch', 'minify-index', 'minify-html', 'styles', 'image']); 
 
